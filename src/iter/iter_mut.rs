795b297e1a6d6d2cd5c8bb0c83ccddbf2ba8afe7
Lucas <24826124+Luro02@users.noreply.github.com>
2021-07-11 12:16:48 +0200

    implement `fmt::Debug` for `IterMut`

--- src/iter/iter_mut.rs
+++ src/iter/iter_mut.rs
@@ -1,12 +1,16 @@
-use core::iter;
+use core::fmt;
 use core::slice;
 
 #[must_use]
-pub struct IterMut<'a, K, V>(iter::Flatten<slice::IterMut<'a, Option<(K, V)>>>);
+pub struct IterMut<'a, K, V>(slice::IterMut<'a, Option<(K, V)>>);
 
 impl<'a, K, V> IterMut<'a, K, V> {
     pub fn new(entries: &'a mut [Option<(K, V)>]) -> Self {
-        Self(entries.iter_mut().flatten())
+        Self(entries.iter_mut())
+    }
+
+    pub(crate) fn iter(&self) -> slice::Iter<'_, Option<(K, V)>> {
+        self.0.as_slice().iter()
     }
 }
 
@@ -14,10 +18,18 @@ impl<'a, K, V> Iterator for IterMut<'a, K, V> {
     type Item = (&'a K, &'a mut V);
 
     fn next(&mut self) -> Option<Self::Item> {
-        if let Some((k, v)) = self.0.next() {
-            Some((k, v))
-        } else {
-            None
+        for entry in self.0.next() {
+            if let Some((k, v)) = entry {
+                return Some((k, v));
+            }
         }
+
+        None
+    }
+}
+
+impl<'a, K: fmt::Debug, V: fmt::Debug> fmt::Debug for IterMut<'a, K, V> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_list().entries(self.0.as_slice().iter()).finish()
     }
 }
