6131c9e4c7e7cdad1e33ae8ae050e84aaef4a716
Lucas <24826124+Luro02@users.noreply.github.com>
2021-07-12 12:06:02 +0200

    implement `FusedIterator`, `DoubleEndedIterator` and `ÈxactSizeIterator` for `IterCircular`

--- src/utils/iter_circular.rs
+++ src/utils/iter_circular.rs
@@ -1,4 +1,4 @@
-use core::iter;
+use core::iter::{self, FusedIterator};
 use core::ops::Range;
 use core::slice;
 
@@ -28,7 +28,21 @@ impl<'a, T> Iterator for IterCircular<'a, T> {
     fn next(&mut self) -> Option<Self::Item> {
         self.iter.next()
     }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        self.iter.size_hint()
+    }
 }
+
+impl<'a, T> DoubleEndedIterator for IterCircular<'a, T> {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        self.iter.next_back()
+    }
+}
+
+impl<'a, T> FusedIterator for IterCircular<'a, T> {}
+
+impl<'a, T> ExactSizeIterator for IterCircular<'a, T> {}
 
 #[cfg(test)]
 mod tests {
@@ -87,11 +101,67 @@ mod tests {
     }
 
     #[test]
-    fn test_circular_abitrary() {
+    fn test_circular_fixed_reverse() {
+        assert_eq!(
+            array_collect::<_, _, 10>(IterCircular::new(0, &[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]).rev()),
+            [
+                Some((9, &9)),
+                Some((8, &8)),
+                Some((7, &7)),
+                Some((6, &6)),
+                Some((5, &5)),
+                Some((4, &4)),
+                Some((3, &3)),
+                Some((2, &2)),
+                Some((1, &1)),
+                Some((0, &0)),
+            ]
+        );
+
+        assert_eq!(
+            array_collect::<_, _, 10>(IterCircular::new(4, &[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]).rev()),
+            [
+                Some((3, &3)),
+                Some((2, &2)),
+                Some((1, &1)),
+                Some((0, &0)),
+                Some((9, &9)),
+                Some((8, &8)),
+                Some((7, &7)),
+                Some((6, &6)),
+                Some((5, &5)),
+                Some((4, &4)),
+            ]
+        );
+    }
+
+    #[test]
+    fn test_circular_arbitrary() {
         let array: [_; 100] = numbered_array();
 
         for i in 0..array.len() {
             for (idx, value) in IterCircular::new(i, &array) {
+                assert_eq!(&idx, value);
+                assert_eq!(&array[idx], value);
+            }
+        }
+    }
+
+    #[test]
+    fn test_circular_arbitrary_reverse() {
+        let array: [_; 100] = numbered_array();
+
+        for i in 0..array.len() {
+            let values: [_; 100] = array_collect(IterCircular::new(i, &array)).map(Option::unwrap);
+            let mut rvalues_expected = values.clone();
+            rvalues_expected.reverse();
+
+            let rvalues: [_; 100] =
+                array_collect(IterCircular::new(i, &array).rev()).map(Option::unwrap);
+
+            assert_eq!(rvalues_expected, rvalues);
+
+            for (idx, value) in values {
                 assert_eq!(&idx, value);
                 assert_eq!(&array[idx], value);
             }
