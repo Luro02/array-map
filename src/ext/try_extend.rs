51cdc34f443657fb982c51b899eb2ca48ff89360
Lucas <24826124+Luro02@users.noreply.github.com>
2021-07-21 11:05:55 +0200

    document `TryExtend` trait

--- src/ext/try_extend.rs
+++ src/ext/try_extend.rs
@@ -1,5 +1,14 @@
+/// Try to extend a collection with the contents of an iterator.
 pub trait TryExtend<A> {
+    /// The error returned if it failed to extend the collection.
     type Error;
 
+    /// Tries to extend the collection with the values from the provided iterator.
+    ///
+    /// # Errors
+    ///
+    /// If the collection runs out of capacity or for example one tries to insert
+    /// elements that should not be inserted.
+    /// This depends mostly on the collection itself.
     fn try_extend<T: IntoIterator<Item = A>>(&mut self, iter: T) -> Result<(), Self::Error>;
 }
