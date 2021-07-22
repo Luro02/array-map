31810c626a5623bda211994638e53e180023b8cc
Lucas <24826124+Luro02@users.noreply.github.com>
2021-07-22 10:35:22 +0200

    implement `Debug`, `ExactSizeIterator` and `FusedIterator` for `IntoIter`

--- src/iter/into_iter.rs
+++ src/iter/into_iter.rs
@@ -1,11 +1,21 @@
-use core::{array, iter};
+use core::array;
+use core::fmt::{self, Debug};
+use core::iter::FusedIterator;
 
 #[must_use]
-pub struct IntoIter<K, V, const N: usize>(iter::Flatten<array::IntoIter<Option<(K, V)>, N>>);
+pub struct IntoIter<K, V, const N: usize>(array::IntoIter<Option<(K, V)>, N>);
 
 impl<K, V, const N: usize> IntoIter<K, V, N> {
-    pub fn new(entries: [Option<(K, V)>; N]) -> Self {
-        Self(array::IntoIter::new(entries).flatten())
+    pub(crate) fn new(entries: [Option<(K, V)>; N]) -> Self {
+        Self(array::IntoIter::new(entries))
+    }
+
+    fn iter(&self) -> impl Iterator<Item = &(K, V)> {
+        self.0.as_slice().iter().flat_map(Option::as_ref)
+    }
+
+    fn len(&self) -> usize {
+        self.iter().count()
     }
 }
 
@@ -13,6 +23,24 @@ impl<K, V, const N: usize> Iterator for IntoIter<K, V, N> {
     type Item = (K, V);
 
     fn next(&mut self) -> Option<Self::Item> {
-        self.0.next()
+        (&mut self.0).flatten().next()
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (self.len(), Some(self.len()))
     }
 }
+
+impl<K, V, const N: usize> fmt::Debug for IntoIter<K, V, N>
+where
+    K: Debug,
+    V: Debug,
+{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.debug_list().entries(self.iter()).finish()
+    }
+}
+
+impl<K, V, const N: usize> ExactSizeIterator for IntoIter<K, V, N> {}
+
+impl<K, V, const N: usize> FusedIterator for IntoIter<K, V, N> {}
