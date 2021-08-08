ffba996e1430a4310b32158aebc0fdb87639c037
Lucas <24826124+Luro02@users.noreply.github.com>
2021-08-08 18:16:27 +0200

    fuzzer: improve `Debug` output of `HasHash`

--- fuzz/fuzz_targets/fuzz_probing.rs
+++ fuzz/fuzz_targets/fuzz_probing.rs
@@ -3,11 +3,21 @@ use array_map::ArrayMap;
 use libfuzzer_sys::arbitrary::{Arbitrary, Unstructured};
 use libfuzzer_sys::fuzz_target;
 use std::collections::HashMap;
+use std::fmt;
 
-#[derive(Debug, PartialEq, Eq, Clone, Copy)]
-struct HasHash(u64, u64);
+#[derive(PartialEq, Eq, Clone, Copy)]
+struct HasHash<const N: usize>(u64, u64);
 
-impl core::hash::Hash for HasHash {
+impl<const N: usize> fmt::Debug for HasHash<N> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_tuple(stringify!(HasHash))
+            .field(&(self.0 % N as u64))
+            .field(&self.1)
+            .finish()
+    }
+}
+
+impl<const N: usize> core::hash::Hash for HasHash<N> {
     fn hash<H>(&self, h: &mut H)
     where
         H: core::hash::Hasher,
@@ -16,7 +26,7 @@ impl core::hash::Hash for HasHash {
     }
 }
 
-impl<'a> Arbitrary<'a> for HasHash {
+impl<'a, const N: usize> Arbitrary<'a> for HasHash<N> {
     fn arbitrary(u: &mut Unstructured<'a>) -> Result<Self, libfuzzer_sys::arbitrary::Error> {
         Ok(Self(u.arbitrary::<_>()?, u.arbitrary::<_>()?))
     }
@@ -46,11 +56,11 @@ impl core::hash::Hasher for Hasher {
 
 type CustomHasher = core::hash::BuildHasherDefault<Hasher>;
 
-const INPUT_SIZE: usize = 3;
-const CAPACITY: usize = 4;
+const INPUT_SIZE: usize = 200;
+const CAPACITY: usize = 200;
 
-fuzz_target!(|data: [(HasHash, usize); INPUT_SIZE]| {
-    let mut map: ArrayMap<HasHash, usize, CAPACITY, CustomHasher> =
+fuzz_target!(|data: [(HasHash<CAPACITY>, usize); INPUT_SIZE]| {
+    let mut map: ArrayMap<HasHash<CAPACITY>, usize, CAPACITY, CustomHasher> =
         ArrayMap::with_build_hasher(CustomHasher::default());
     let mut hash_map = HashMap::with_capacity_and_hasher(CAPACITY, CustomHasher::default());
 
