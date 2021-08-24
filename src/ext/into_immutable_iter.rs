431cb4b1554342cb9206eab2f4ef5896986ba1e4
Lucas <24826124+Luro02@users.noreply.github.com>
2021-08-24 11:11:10 +0200

    test `IntoImmutableIter` trait impls

--- src/ext/into_immutable_iter.rs
+++ src/ext/into_immutable_iter.rs
@@ -31,3 +31,64 @@ impl<T, const N: usize> IntoImmutableIter<T> for array::IntoIter<T, N> {
         self.as_slice().iter()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use crate::ext::IteratorExt;
+
+    #[test]
+    fn test_slice_mut() {
+        let mut array = [1, 2, 3];
+        let slice: &mut [i32] = &mut array[1..];
+        let mut iter_mut = slice.iter_mut();
+
+        assert_eq!(
+            iter_mut.iter().try_collect::<[Option<_>; 3]>().unwrap(),
+            [Some(&2), Some(&3), None]
+        );
+        assert_eq!(iter_mut.next(), Some(&mut 2));
+
+        assert_eq!(
+            iter_mut.iter().try_collect::<[Option<_>; 3]>().unwrap(),
+            [Some(&3), None, None]
+        );
+        assert_eq!(iter_mut.next(), Some(&mut 3));
+
+        assert_eq!(
+            iter_mut.iter().try_collect::<[Option<_>; 3]>().unwrap(),
+            [None, None, None]
+        );
+        assert_eq!(iter_mut.next(), None);
+    }
+
+    #[test]
+    fn test_array() {
+        let mut iter = array::IntoIter::new([1, 2, 3]);
+
+        assert_eq!(
+            iter.iter().try_collect::<[Option<_>; 4]>().unwrap(),
+            [Some(&1), Some(&2), Some(&3), None]
+        );
+        assert_eq!(iter.next(), Some(1));
+
+        assert_eq!(
+            iter.iter().try_collect::<[Option<_>; 4]>().unwrap(),
+            [Some(&2), Some(&3), None, None]
+        );
+        assert_eq!(iter.next(), Some(2));
+
+        assert_eq!(
+            iter.iter().try_collect::<[Option<_>; 4]>().unwrap(),
+            [Some(&3), None, None, None]
+        );
+        assert_eq!(iter.next(), Some(3));
+
+        assert_eq!(
+            iter.iter().try_collect::<[Option<_>; 4]>().unwrap(),
+            [None, None, None, None]
+        );
+        assert_eq!(iter.next(), None);
+    }
+}
