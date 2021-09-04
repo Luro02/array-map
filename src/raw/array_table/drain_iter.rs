fc9264be1ac8f40a2efe4b483047d3f21afa8c9c
Lucas <24826124+Luro02@users.noreply.github.com>
2021-09-04 13:19:18 +0200

    implement `FusedIterator`, `Debug` and `ToIter` for `DrainIter`

--- src/raw/array_table/drain_iter.rs
+++ src/raw/array_table/drain_iter.rs
@@ -1,11 +1,14 @@
-use core::array;
+use core::iter::FusedIterator;
+use core::{array, fmt, iter, slice};
+
+use crate::ext::ToIter;
 
 #[must_use = "iterators are lazy and do nothing unless consumed"]
 pub struct DrainIter<T, const N: usize>(array::IntoIter<Option<T>, N>);
 
 impl<T, const N: usize> DrainIter<T, N> {
     pub(crate) fn new(data: [Option<T>; N]) -> Self {
-        Self(array::IntoIter::new(data))
+        Self(data.into_iter())
     }
 }
 
@@ -14,5 +17,24 @@ impl<T, const N: usize> Iterator for DrainIter<T, N> {
 
     fn next(&mut self) -> Option<Self::Item> {
         self.0.by_ref().flatten().next()
+    }
+}
+
+impl<T, const N: usize> FusedIterator for DrainIter<T, N> {}
+
+impl<T, const N: usize> ToIter<T> for DrainIter<T, N> {
+    type Iter<'a>
+    where
+        T: 'a,
+    = iter::Flatten<slice::Iter<'a, Option<T>>>;
+
+    fn iter(&self) -> Self::Iter<'_> {
+        self.0.iter().flatten()
+    }
+}
+
+impl<T: fmt::Debug, const N: usize> fmt::Debug for DrainIter<T, N> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_list().entries(self.iter()).finish()
     }
 }
